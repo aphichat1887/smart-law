5264f0d25cf671e905203ce400f2af681a05eb35
ApiChok <dhnl96067@gmail.com>
2026-09-16 13:13:04 +0700

    frontend + backend

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface LoginResponse {
     success: boolean;
@@ -13,7 +14,7 @@ export interface LoginResponse {
 })
 export class AuthService {
 
-    private apiUrl = 'http://localhost:8000/api';
+    private apiUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://prepared-pointed-practices-electric.trycloudflare.com/api',
+  
+};
